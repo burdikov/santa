196cd0fc7b2834681df8dc1d2c223ce1b9b705f2
Leonid Burdikov <leonid.b.d@gmail.com>
2018-11-27 20:14:09 +0300

    Remove debug code

--- Santa/Startup.cs
+++ Santa/Startup.cs
@@ -30,8 +30,6 @@ namespace Santa
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            System.Console.WriteLine("hello from configureservices");
-            System.Console.WriteLine(Configuration["connstr"]);
             services.AddDbContext<SantaDbContext>(options => {
                 options.UseSqlServer(Configuration["connstr"]);
             });
